0f1485446081a8d3427f5a2bf0382e5e34a03c9f
rebecaaras <rebecaaraujo2013@gmail.com>
2024-11-14 11:49:41 -0300

    first commit: instruções do teste e set-up inicial

---
+++ src/1-is-slot-available/is-slot-available.ts
@@ -0,0 +1,6 @@
+import { CalendarAvailability, CalendarSlot } from '../types';
+
+export const isSlotAvailable = (availability: CalendarAvailability, slot: CalendarSlot): boolean => {
+  // Your code goes here
+  return false;
+};
