a3f41e3110a7dadf424071fd090e77da154c30aa
rebecaaras <rebecaaraujo2013@gmail.com>
2024-11-15 19:47:13 -0300

    new: Check if event day is available

--- src/1-is-slot-available/is-slot-available.ts
+++ src/1-is-slot-available/is-slot-available.ts
@@ -1,6 +1,15 @@
+import { addMinutes, getDay } from 'date-fns';
 import { CalendarAvailability, CalendarSlot } from '../types';
+import { Weekday } from '../types';
+
 
 export const isSlotAvailable = (availability: CalendarAvailability, slot: CalendarSlot): boolean => {
-  // Your code goes here
-  return false;
+  const doctorSchedule = availability.include;
+  const eventWeekday: Weekday = getDay(slot.start) 
+  /** Check if day of event is available*/
+  const isDayAvailable = () => {
+    return doctorSchedule.some(timeSlot => timeSlot.weekday === eventWeekday)
+  }
+
+  return isDayAvailable()
 };
