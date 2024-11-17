ffe4e5677919f3f57ceea916ab21d555fb0ac732
rebecaaras <rebecaaraujo2013@gmail.com>
2024-11-17 19:57:14 -0300

    new: Add availableScheduleDay function

--- src/1-is-slot-available/is-slot-available.ts
+++ src/1-is-slot-available/is-slot-available.ts
@@ -1,15 +1,33 @@
-import { addMinutes, getDay } from 'date-fns';
+import { addMinutes, getDay, getHours, set, toDate } from 'date-fns';
 import { CalendarAvailability, CalendarSlot } from '../types';
-import { Weekday } from '../types';
-
+import { Weekday, Time } from '../types';
 
 export const isSlotAvailable = (availability: CalendarAvailability, slot: CalendarSlot): boolean => {
+  //types
+  type Workday = {
+    weekday: Weekday;
+    range: [Time, Time];
+}
+
+  //variables
   const doctorSchedule = availability.include;
-  const eventWeekday: Weekday = getDay(slot.start) 
+  const eventDate = slot.start
+  const eventWeekday: Weekday = getDay(eventDate) 
+  
   /** Check if day of event is available*/
-  const isDayAvailable = () => {
-    return doctorSchedule.some(timeSlot => timeSlot.weekday === eventWeekday)
+  function isDayAvailable(): boolean {
+    return doctorSchedule.some(workday => workday.weekday === eventWeekday)
+  }
+  
+  function availableScheduleDay(): Workday | undefined {
+    return doctorSchedule.find((workday) => {
+      workday.range[0].hours <= getHours(eventDate)
+      console.log(workday);
+    })};
+    
+  if (isDayAvailable()){
+    console.log(availableScheduleDay())
   }
 
-  return isDayAvailable()
+  return false
 };
