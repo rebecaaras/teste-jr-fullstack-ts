0ae24149f5a124928e98296d5cf7c5c387e014f1
rebecaaras <rebecaaraujo2013@gmail.com>
2024-11-17 19:54:19 -0300

    new: Add function isSlotAvailable

--- src/1-is-slot-available/is-slot-available.ts
+++ src/1-is-slot-available/is-slot-available.ts
@@ -1,15 +1,35 @@
-import { addMinutes, getDay } from 'date-fns';
+import { addMinutes, getDay, isWithinInterval, setHours, setMinutes } from 'date-fns';
 import { CalendarAvailability, CalendarSlot } from '../types';
-import { Weekday } from '../types';
+import { Weekday, Time } from '../types';
 
-
+/**Verifies if the slot of the event is an available time in schedule. */
 export const isSlotAvailable = (availability: CalendarAvailability, slot: CalendarSlot): boolean => {
-  const doctorSchedule = availability.include;
-  const eventWeekday: Weekday = getDay(slot.start) 
-  /** Check if day of event is available*/
-  const isDayAvailable = () => {
-    return doctorSchedule.some(timeSlot => timeSlot.weekday === eventWeekday)
+  type Workday = {
+    weekday: Weekday;
+    range: [Time, Time];
   }
 
-  return isDayAvailable()
+  const doctorSchedule: Workday[] = availability.include;
+  const eventStartTime: Date = slot.start
+  const eventEndTime: Date = addMinutes(eventStartTime, slot.durationM)
+  
+  const workday: Workday | undefined =  doctorSchedule.find(workday => workday.weekday === getDay(eventStartTime))
+  if (!workday){
+    return false
+  } 
+
+  //convert workday interval available to Date
+  let workdayStartTime: Date = setHours(eventStartTime, workday.range[0].hours)
+  workdayStartTime = setMinutes(workdayStartTime, workday.range[0].minutes)
+
+  let workdayEndTime: Date = setHours(eventStartTime, workday.range[1].hours)
+  workdayEndTime = setMinutes(workdayEndTime, workday.range[1].minutes)
+  
+  if (isWithinInterval(eventStartTime, {start: workdayStartTime, end: workdayEndTime}) 
+    && (isWithinInterval(eventEndTime, {start: workdayStartTime, end: workdayEndTime}))){
+      return true
+  } else {
+    return false
+  }
+
 };
