b7de8fbdc1ce373f0ac47c0caf0d9f1606381870
rebecaaras <rebecaaraujo2013@gmail.com>
2024-11-27 15:38:40 -0300

    new: Add function isSlotAvailableWithBuffer

--- src/3-is-slot-available-with-buffer/is-slot-available-with-buffer.ts
+++ src/3-is-slot-available-with-buffer/is-slot-available-with-buffer.ts
@@ -1,10 +1,23 @@
+import { addMinutes, areIntervalsOverlapping, subMinutes } from 'date-fns';
 import { CalendarAvailability, CalendarEvent, CalendarSlot } from '../types';
+import { isSlotAvailable } from '../1-is-slot-available/is-slot-available';
 
 export const isSlotAvailableWithBuffer = (
   availability: CalendarAvailability,
   events: Array<CalendarEvent>,
   slot: CalendarSlot,
 ): boolean => {
-  // Your code goes here
-  return false;
+  if (!isSlotAvailable(availability, slot)){
+    return false;
+  }
+
+  const isOverlapping = events.some(event => {
+    const bufferBefore: number = event.buffer?.before?? 0;
+    const bufferAfter: number = event.buffer?.after?? 0;
+
+    return areIntervalsOverlapping({start: slot.start, end: addMinutes(slot.start, slot.durationM)},
+                                   {start: subMinutes(event.start, bufferBefore), end: addMinutes(event.end, bufferAfter)});
+  })
+
+  return !isOverlapping;
 };
